5bd5d6cb2bff050eeb1d420e766eeb81ec4869fd
naty55 <64107292+naty55@users.noreply.github.com>
2024-04-30 00:52:51 +0300

    Redirect output to null and set echo messages

--- start_bot.sh
+++ start_bot.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-git fetch https://github.com/naty55/NautzBot.git
-pip install -r requirements.txt
+echo "Fetching latest version from master..." 
+git fetch https://github.com/naty55/NautzBot.git > /dev/null 2>&1 
+
+echo "Installing dependencies..."
+pip install -r requirements.txt > /dev/null
 
 bot_token=`cat token.txt`
 
+echo "Starting Bot..."
 python3 NautzBot.py
